02438cba25e752e689b5aa454545b290373f7024
panda086lowk <sakuarean@gmail.com>
2026-06-10 13:56:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+function toggleDropdown() {
+  const menu = document.getElementById("Pmenu");
+  
+  if(menu.style.display === "block"){
+    menu.style.display = "none";
+  }
+  else{
+    menu.style.display = "block";
+  }
+}
